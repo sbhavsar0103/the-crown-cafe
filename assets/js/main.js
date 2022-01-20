c19abf77195610db0afa293b5c4e3f6d0820d0ce
sbhavsar0103 <sbhavsar282@gmail.com>
2022-01-19 22:02:32 -0800

    commited on 20/1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,7 @@
+;(function () {
+  'use strict'
+  // Easy selector helper function
 
-(function() {
-  "use strict";
-    // Easy selector helper function
-  
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -13,57 +12,59 @@
   }
 
   //  Easy event listener function
-   
+
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
     if (selectEl) {
       if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
+        selectEl.forEach((e) => e.addEventListener(type, listener))
       } else {
         selectEl.addEventListener(type, listener)
       }
     }
   }
 
-    // Easy on scroll event listener 
-   
+  // Easy on scroll event listener
+
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
 
-    // Navbar links active state on scroll
-   
+  // Navbar links active state on scroll
+
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
+    navbarlinks.forEach((navbarlink) => {
       if (!navbarlink.hash) return
       let section = select(navbarlink.hash)
       if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+      if (
+        position >= section.offsetTop &&
+        position <= section.offsetTop + section.offsetHeight
+      ) {
         navbarlink.classList.add('active')
       } else {
         navbarlink.classList.remove('active')
       }
     })
   }
-    /**
+  /**
    * Scrolls to an element with header offset
    */
-     const scrollto = (el) => {
-      let header = select('#header')
-      let offset = header.offsetHeight
-  
-      let elementPos = select(el).offsetTop
-      window.scrollTo({
-        top: elementPos - offset,
-        behavior: 'smooth'
-      })
-    }
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth',
+    })
+  }
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
 
- 
   let selectHeader = select('#header')
   let selectTopbar = select('#topbar')
   if (selectHeader) {
@@ -83,33 +84,43 @@
     window.addEventListener('load', headerScrolled)
     onscroll(document, headerScrolled)
   }
-    // Mobile nav toggle
-  on('click', '.mobile-nav-toggle', function(e) {
+  // Mobile nav toggle
+  on('click', '.mobile-nav-toggle', function (e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
-    // Mobile nav dropdowns activate 
-  on('click', '.navbar .dropdown > a', function(e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault()
-      this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-  }, true)
-  //  Scrool with ofset on links with a class name .scrollto
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
-      let navbar = select('#navbar')
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
+  // Mobile nav dropdowns activate
+  on(
+    'click',
+    '.navbar .dropdown > a',
+    function (e) {
+      if (select('#navbar').classList.contains('navbar-mobile')) {
+        e.preventDefault()
+        this.nextElementSibling.classList.toggle('dropdown-active')
       }
-      scrollto(this.hash)
-    }
-  }, true)
+    },
+    true,
+  )
+  //  Scrool with ofset on links with a class name .scrollto
+  on(
+    'click',
+    '.scrollto',
+    function (e) {
+      if (select(this.hash)) {
+        e.preventDefault()
+        let navbar = select('#navbar')
+        if (navbar.classList.contains('navbar-mobile')) {
+          navbar.classList.remove('navbar-mobile')
+          let navbarToggle = select('.mobile-nav-toggle')
+          navbarToggle.classList.toggle('bi-list')
+          navbarToggle.classList.toggle('bi-x')
+        }
+        scrollto(this.hash)
+      }
+    },
+    true,
+  )
   //  Scroll with ofset on page load with hash links in the url
   window.addEventListener('load', () => {
     if (window.location.hash) {
@@ -117,88 +128,96 @@
         scrollto(window.location.hash)
       }
     }
-  });
+  })
   // menu filter
   window.addEventListener('load', () => {
-    let menuContainer = select('.menu-container');
+    let menuContainer = select('.menu-container')
     if (menuContainer) {
       let menuIsotope = new Isotope(menuContainer, {
         itemSelector: '.menu-item',
-        layoutMode: 'fitRows'
-      });
-      let menuFilters = select('#menu-flters li', true);
-      on('click', '#menu-flters li', function(e) {
-        e.preventDefault();
-        menuFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
-  
-        menuIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        menuIsotope.on('arrangeComplete', function() {
-          AOS.refresh()
-        });
-      }, true);
+        layoutMode: 'fitRows',
+      })
+      let menuFilters = select('#menu-flters li', true)
+      on(
+        'click',
+        '#menu-flters li',
+        function (e) {
+          e.preventDefault()
+          menuFilters.forEach(function (el) {
+            el.classList.remove('filter-active')
+          })
+          this.classList.add('filter-active')
+
+          menuIsotope.arrange({
+            filter: this.getAttribute('data-filter'),
+          })
+          menuIsotope.on('arrangeComplete', function () {
+            AOS.refresh()
+          })
+        },
+        true,
+      )
     }
-  
-  });
-  //  Initiate glightbox 
+  })
+  //  Initiate glightbox
   const glightbox = GLightbox({
-    selector: '.glightbox'
-  });
-    // Events slider
+    selector: '.glightbox',
+  })
+  // Events slider
   new Swiper('.events-slider', {
     speed: 600,
     loop: true,
     autoplay: {
       delay: 5000,
-      disableOnInteraction: false
+      disableOnInteraction: false,
     },
     slidesPerView: 'auto',
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
-      clickable: true
-    }
-  });
-    // Testimonials slider
+      clickable: true,
+    },
+  })
+  // Testimonials slider
   new Swiper('.testimonials-slider', {
     speed: 600,
     loop: true,
     autoplay: {
       delay: 5000,
-      disableOnInteraction: false
+      disableOnInteraction: false,
     },
     slidesPerView: 'auto',
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
-      clickable: true
+      clickable: true,
     },
     breakpoints: {
       320: {
         slidesPerView: 1,
-        spaceBetween: 20
+        spaceBetween: 20,
       },
       1200: {
         slidesPerView: 3,
-        spaceBetween: 20
-      }
-    }
-  });
-  //  Initiate gallery lightbox 
+        spaceBetween: 20,
+      },
+    },
+  })
+  //  Initiate gallery lightbox
   const galleryLightbox = GLightbox({
-    selector: '.gallery-lightbox'
-  });
-    // Animation on scroll
+    selector: '.gallery-lightbox',
+  })
+  // Animation on scroll
   window.addEventListener('load', () => {
     AOS.init({
       duration: 1000,
       easing: 'ease-in-out',
       once: true,
-      mirror: false
+      mirror: false,
     })
-  });
+  })
 })()
+
+function myFunction(x) {
+  x.classList.toggle('fa fa-star')
+}
